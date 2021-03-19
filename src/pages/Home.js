0987c7f69d59a40156f8b2b8b3e758df404c82f2
DevAlexander_Bi07 <dev.alexander.bi07@gmail.com>
2021-03-18 18:57:20 -0600

    Se agrego scroll vertical a las imagenes en la vista Home y se agrego efecto "bounce" al botón "Conoce más"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,14 @@ import '../pages/home.css'
 import derecha1 from '../Imagenes/derecha1.png';
 import derecha2 from '../Imagenes/derecha2.png';
 import derecha3 from '../Imagenes/derecha3.png';
+import { Carousel } from "react-responsive-carousel";
 
 
 
 const Home = ()=>{
-
-
-
     return (
-      <div class="row">
-        <div class="col s12 m8 l8">
+      <div className="row">
+        <div className="col s8 m8 l8">
 
         <div className="contenedorcentral">
      
@@ -32,55 +30,36 @@ const Home = ()=>{
               <p className="flecha"></p> *
         </div>
             
-         
-
-
         </div>
-        <div class="col s12 m4 l4">
+        <div className="col s12 m4 l4">
 
-            <div class="contenedorImagenesHome">
-
-              
-                  <table className="tablita">
-                    
-
-                    <tbody>
-                      <tr>
-                        <td> <img src={derecha1} class="responsive-img"  className="imagenDerecha1"/></td>
-                      
-                      </tr>
-                      <tr>
-                        <td> <img src={derecha2} class="responsive-img"   className="imagenDerecha2"/></td>
-                    
-                      </tr>
-                      <tr>
-                        <td> <img src={derecha3} class="responsive-img"  className="imagenDerecha3"/></td>
-                        
-                      </tr>
-                    </tbody>
-                  </table>
-              
-                {/* <div class="row">
-                
-                </div>
-
-              
-                 <div class="row">
-                 
-                </div>
-
-                <div class="row">
-                 
-                </div>  */}
+            <div className="contenedorImagenesHome">
+            <Carousel className="tablita" 
+                      slidesPerPage={2}
+                      slidesPerScroll={2}
+                      autoPlay={true} 
+                      interval={3000} 
+                      animationSpeed={1500}
+                      showThumbs={false}
+                      showStatus={false} 
+                      showArrows={false} 
+                      dynamicHeight={false} 
+                      axis={'vertical'} 
+                      infinite
+                      infiniteLoop={true}
+                      offset={50}
+                      stopOnHover={false}>
+              <div className="imagenDerecha1 responsive-img"><img src={derecha1} /></div>
+              <div className="imagenDerecha2 responsive-img"><img src={derecha2} /></div>
+              <div className="imagenDerecha3 responsive-img"><img src={derecha3} /></div>
+            </Carousel>
+                  
             </div>
         </div>
        
-    
-
       </div>
 
-      )
+      );
 }
-
 
   export default Home;
