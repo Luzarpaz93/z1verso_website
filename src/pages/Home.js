6c1ffdfadbe5f460e88742fab23e258e350491e1
DevAlexander_Bi07 <dev.alexander.bi07@gmail.com>
2021-04-27 20:00:29 -0500

    Se alargó la duración de las animaciones y se agregaron efectos de animación al hacer clic sobre los botones y flechas del home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,10 @@ const Home = ()=>{
 
   });
 
-
+  const [animate, setAnimate] = useState({
+    section1: 1,
+    section2: 1
+  });
 
     return (
       <div className="row">
@@ -38,11 +41,12 @@ const Home = ()=>{
                 videojuegos.
               </p>
               <br />
-              <a href="#conocemas" class="boton zoom-on-load">
+              <a href="#conocemas" class="boton zoom-on-load" onClick={() => setAnimate({section1: 1, section2: 0})}>
                 Conoce más
               </a>
               <br />
-              <a id="primera-flecha zoom-on-load" href="#conocemas" style={{display: "table", width: "5%", margin: "0 auto"}}>
+              <a id="primera-flecha zoom-on-load" href="#conocemas" 
+              style={{display: "table", width: "5%", margin: "0 auto"}} onClick={() => setAnimate({section1: 1, section2: 0})}>
                 <p className="flecha zoom-on-load"></p>
               </a>
             </div>
@@ -94,15 +98,16 @@ const Home = ()=>{
         <div className="row" id="conocemas">
           <div className="col s12 m6 l6">
             <div className="contenedorcentral">
-              <p className="tituloParrafo">Misión</p>
-              <img className="linea-img zoom-on-load" src={linea} />
-              <p className="textoCenter slide-in-animate">
+              <p className="tituloParrafo stateful-slide-left" slide={animate.section1}>Misión</p>
+              <img className="linea-img stateful-zoom" src={linea} animate={animate.section1}/>
+              <p className="textoCenter stateful-slide-left" slide={animate.section1}
+              onAnimationEnd={() => setAnimate({section1: 0, section2: 0})}>
                 Establecer un vínculo cercano con nuestros clientes y
                 proveedores, que permita el máximo desempeño de sus herramientas
                 tecnológicas de trabajo, implementando el mejor costo-beneficio.
               </p>
-              <img src={vinoculares} className="vinoculares hide-on-med-and-up zoom-on-load" />
-              <img src={vinoculares} className="vinoculares hide-on-small-only zoom-on-load" />
+              <img src={vinoculares} className="vinoculares hide-on-med-and-up stateful-zoom" animate={animate.section1} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})}/>
+              <img src={vinoculares} className="vinoculares hide-on-small-only stateful-zoom" animate={animate.section1} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})}/>
               <br></br>
               <br></br>
               <br></br>
@@ -112,15 +117,15 @@ const Home = ()=>{
 
           <div className="col s12 m6 l6">
             <div className="contenedorcentral">
-              <img src={cuete} className="cuete hide-on-small-only zoom-on-load" />
-              <p className="tituloParrafo">Visión</p>
-              <img className="linea-img zoom-on-load" src={linea} />
-              <p className="textoCenter slide-in-animate">
+              <img src={cuete} className="cuete hide-on-small-only stateful-zoom" animate={animate.section1} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})}/>
+              <p className="tituloParrafo stateful-slide-right" slide={animate.section1}>Visión</p>
+              <img className="linea-img stateful-zoom" src={linea} animate={animate.section1} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})}/>
+              <p className="textoCenter stateful-slide-right" slide={animate.section1} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})}>
                 Ser la empresa de ensamble personalizado de computadoras número
                 1 en México, con la mayor calidad en piezas y a un precio
                 accesible para poner al alcance de todos la tecnología.
               </p>
-              <img src={cuete} className="hide-on-med-and-up zoom-on-load" />
+              <img src={cuete} className="hide-on-med-and-up stateful-zoom" animate={animate.section1} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})} />
               <br />
             </div>
           </div>
@@ -129,9 +134,9 @@ const Home = ()=>{
         {/*para el icono de la flecha */}
         <div className="row">
         <div className="col s12 m12 l12"></div>
-          <div className="flecha-container zoom-on-load">
-            <a href="#valores" style={{display: "table", width: "5%", margin: "0 auto"}}>
-              <p className="flecha zoom-on-load"></p>
+          <div className="flecha-container stateful-zoom">
+            <a href="#valores" style={{display: "table", width: "5%", margin: "0 auto"}} onClick={() => setAnimate({section1: 0, section2: 1})}>
+              <p className="flecha stateful-zoom"></p>
             </a>
           </div>
         </div>
@@ -140,25 +145,25 @@ const Home = ()=>{
         <div className="row custom-flex" id="valores">
           <div className="col s12 m7 l7">
             <div className="contenedorcentral">
-              <p className="tituloParrafo">Valores</p>
-              <img className="linea-img zoom-on-load" src={linea} />
-              <p className="textoCenterValores slide-in-animate">
+              <p className="tituloParrafo stateful-slide-left" slide={animate.section2}>Valores</p>
+              <img className="linea-img stateful-zoom" src={linea} animate={animate.section2} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})} />
+              <p className="textoCenterValores stateful-slide-left" slide={animate.section2}>
                 Trabajo en equipo: La mejor manera de alcanzar el éxito es
                 trabajando todo juntos.
               </p>
-              <p className="textoCenterValores slide-in-animate">
+              <p className="textoCenterValores stateful-slide-left" slide={animate.section2}>
                 Esfuerzo: Respetar el empeño puesto por los demás y sumar el
                 nuestro para alcanzar objetivos.
               </p>
-              <p className="textoCenterValores slide-in-animate">
+              <p className="textoCenterValores stateful-slide-left" slide={animate.section2}>
                 Honestidad: Expresamos nuestras ideas a los demás de forma
                 respetuosa, no ocultamos información de nuestros compañeros.
               </p>
-              <p className="textoCenterValores slide-in-animate">
+              <p className="textoCenterValores stateful-slide-left" slide={animate.section2}>
                 Equidad: Tratar con respeto a los demás, sin importar sus
                 creencias o género.
               </p>
-              <p className="textoCenterValores slide-in-animate">
+              <p className="textoCenterValores stateful-slide-left" slide={animate.section2}>
                 Lealtad: Con los clientes y proveedores, para buscar lo mejor
                 para todas las partes interesadas.
               </p>
@@ -167,7 +172,7 @@ const Home = ()=>{
 
           <div className="col s12 m5 l5 height-diamante">
             {<span className="align-helper"></span>}
-            <img className="align-diamante zoom-on-load" src={diamante} />
+            <img className="align-diamante stateful-zoom" src={diamante} animate={animate.section2} onAnimationEnd={() => setAnimate({section1: 0, section2: 0})}/>
             {/*<br></br>
                       <br></br>
                       <br></br>
