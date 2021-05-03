b68332ceda0a4e9056fa3fd452c67e629ec189e1
DevAlexander_Bi07 <dev.alexander.bi07@gmail.com>
2021-05-03 00:36:13 -0500

    Se agrego formulario requerido a la vista "Gaming.js"

---
+++ src/Tabs/TabsCreative.js
@@ -0,0 +1,257 @@
+import React, { Component } from "react";
+import M from "materialize-css";
+import emailjs from "emailjs-com";
+import "materialize-css/dist/css/materialize.min.css";
+import { ToastContainer, toast } from "material-react-toastify";
+import "material-react-toastify/dist/ReactToastify.css";
+
+class TabsCreative extends Component {
+  formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "MXN",
+  });
+
+  notify = () => toast.error("Los campos con asterisco son obligatorios");
+  notify_mail = () => toast.error("Capture una dirección valida de correo");
+
+  componentDidMount() {
+    this.instance = M.Tabs.init(this.Tabs);
+    this.instance = M.Tabs.getInstance(this.Tabs);
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
+    this.regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  }
+
+  siguienteOnCLick() {
+    this.instance.select("test-swipe-2");
+  }
+
+  enviarOnCLick() {
+    if (this.Email && this.Nombre && this.DataJuegos && this.CaracAdicional) {
+      if (this.Email.match(this.regEmail)) {
+        let mensajeCompleto =
+          "Nombre: " +
+          this.Nombre +
+          "\nNormalmente juego: " +
+          this.DataJuegos +
+          "\nCorreo eléctronico: " +
+          this.Email +
+          "\n" +
+          "Presupuesto: " +
+          ((this.Presupuesto) ? this.Presupuesto : "No Especificado") +
+          "\n\nCaracteristicas Adicionales:\n" +
+          (this.CaracAdicional ?? "");
+
+        const templateParams = {
+          from_name: this.Nombre,
+          message: mensajeCompleto,
+          to_name: "Ziverso",
+        };
+
+        console.log(mensajeCompleto);
+        /*emailjs
+          .send(
+            "service_uzqm4x4",
+            "template_2r31rfd",
+            templateParams,
+            "user_Sh6GL9McCN2ZjRzffcdQY"
+          )
+          .then(
+            (response) => {
+              console.log("SUCCESS!", response.status, response.text);
+            },
+            (err) => {
+              console.log("FAILED...", err);
+            }
+          );*/
+      } else {
+        this.notify_mail();
+      }
+    } else {
+      this.notify();
+    }
+  }
+
+  //setname(e.target.value)
+  onChangeNombre(nombre) {
+    this.Nombre = nombre;
+  }
+
+  onChangeEmail(mail) {
+    this.Email = mail;
+  }
+
+  onChangePresupuesto(option) {
+    //this.Presupuesto = budget;
+    let data = parseInt(option ?? 0);
+    switch(data){
+      case 0:
+        this.Presupuesto = "";
+        break;
+      case 1:
+        this.Presupuesto = "10,000 - 15,000";
+        break;
+      case 2:
+        this.Presupuesto = "15,000 - 20,000";
+        break;
+      case 3:
+        this.Presupuesto = "25,000 - más";
+        break;
+
+    }
+    console.log(option)
+  }
+
+  onChangeDetalle(adicional) {
+    this.CaracAdicional = adicional;
+  }
+
+  onChangeProgramas(programas_usados) {
+    this.DataProgramas = programas_usados;
+  }
+
+  onChangeUsoPc(usosPc){
+    this.UsoPrincipalPc = usosPc;
+  }
+
+  render() {
+    return (
+      <>
+        <ul
+          ref={(Tabs) => {
+            this.Tabs = Tabs;
+          }}
+          id="tabs-swipe-demo"
+          className="tabs"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+        >
+          <li className="tab col s3">
+            <a href="#test-swipe-1">Info 1</a>
+          </li>
+          <li className="tab col s3">
+            <a href="#test-swipe-2">Info 2</a>
+          </li>
+        </ul>
+
+        <div id="test-swipe-1" className="col s12 m12 l12 black">
+          <div
+            className="input-field col s12 m12 l12"
+            style={{ margin: "none", padding: "8px" }}
+          >
+            <input
+              id="nombre"
+              type="text"
+              className="white-text"
+              autocomplete="off"
+              onChange={(e) => this.onChangeNombre(e.target.value)}
+            />
+            <label htmlFor="nombre">Nombre*</label>
+          </div>
+          <div
+            className="input-field col s12 m12 l12"
+            style={{ margin: "none", padding: "8px" }}
+          >
+            <input
+              id="email"
+              type="email"
+              className="white-text"
+              autocomplete="off"
+              onChange={(e) => this.onChangeEmail(e.target.value)}
+            />
+            <label htmlFor="email">Email*</label>
+          </div>
+
+          <div
+            className="input-field col s12 m12 l12"
+            style={{ margin: "none", padding: "8px" }}
+          >
+            <input
+              id="jegos"
+              type="text"
+              className="white-text"
+              autocomplete="off"
+              onChange={(e) => this.onChangeProgramas(e.target.value)}
+            />
+            <label htmlFor="juegos">¿Que programas usas normalmente?*</label>
+          </div>
+
+          <div className="col s6 offset-s4 m6 offset-m3 l6 offset-l4">
+            <button
+              className="botonGaming pointer"
+              onClick={() => this.siguienteOnCLick()}
+            >
+              Siguiente
+            </button>
+          </div>
+        </div>
+        <div id="test-swipe-2" className="col s12 m12 l12 black">
+          <div
+            class="input-field col s12 m12 l12"
+            style={{ margin: "none", padding: "8px" }}
+          >
+            <i class="material-icons prefix icon-white">paid</i>
+            <select id="select-presupuesto" style={{color: "#ffffff"}}
+            onChange={e => this.onChangePresupuesto(e.target.value)}>
+              <option defaultValue="0" value="0">
+                Opcional
+              </option>
+              <option value="1">10,000 - 15,000</option>
+              <option value="2">15,000 - 20,000</option>
+              <option value="3">25,000 - más</option>
+            </select>
+            <label htmlFor="select-presupuesto">Presupuesto</label>
+            {/*<input
+              id="presupuesto"
+              type="number"
+              className="white-text"
+              autocomplete="off"
+              onChange={(e) => this.onChangePresupuesto(e.target.value)}
+            />
+            <label htmlFor="presupuesto">Presupuesto</label>*/}
+          </div>
+          <div
+            className="input-field col s12 m12 l12"
+            style={{ margin: "none", padding: "8px" }}
+          >
+            <input
+              id="jegos"
+              type="text"
+              className="white-text"
+              autocomplete="off"
+              onChange={(e) => this.onChangeUsoPc(e.target.value)}
+            />
+            <label htmlFor="juegos">Uso principal de la PC*</label>
+          </div>
+          <div
+            className="input-field col s12 m12 l12"
+            style={{ margin: "none", padding: "8px" }}
+          >
+            <textarea
+              id="caracteristicas_adicionales"
+              class="materialize-textarea"
+              className="white-text"
+              autocomplete="off"
+              rows="6"
+              cols="50"
+              onChange={(e) => this.onChangeDetalle(e.target.value)}
+            ></textarea>
+            <label htmlFor="caracteristicas_adicionales">
+              Caracteristicas adicionales*
+            </label>
+          </div>
+          <div className="col s6 offset-s4 m6 offset-m3 l6 offset-l5">
+            <button
+              className="botonGaming pointer"
+              onClick={() => this.enviarOnCLick()}
+            >
+              Enviar
+            </button>
+          </div>
+        </div>
+        <ToastContainer position="top-center" autoClose={5000} />
+      </>
+    );
+  }
+}
+
+export default TabsCreative;
--- src/pages/Gaming.js
+++ src/pages/Gaming.js
@@ -1,68 +1,76 @@
-import React, { useState, useEffect, useLayoutEffect }  from 'react';
-import logo from '../Imagenes/logoGaming.png';
-import control from '../Imagenes/ControlGaleria.png';
-import linea from '../Imagenes/line.png';
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import logo from "../Imagenes/logoGaming.png";
+import control from "../Imagenes/ControlGaleria.png";
+import linea from "../Imagenes/line.png";
+import TabsGaming from "../Tabs/TabsGaming.js";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-import '../pages/Gaming.css'
+import "../pages/Gaming.css";
 
-
-
-const Gaming = ()=>{
-
+const Gaming = () => {
   useLayoutEffect(() => {
-    document.body.className = 'gaming-background';
-    return () => { document.body.className = ''; }
+    document.body.className = "gaming-background";
+    return () => {
+      document.body.className = "";
+    };
   }, []);
 
   useEffect(() => {
     localStorage.clear();
-    localStorage.setItem('seleccion', "gaming")
+    localStorage.setItem("seleccion", "gaming");
   });
 
-
-    return (
+  return (
+    <React.Fragment>
       <div class="row">
-      {/*<div class="col s12 m4 l2">
+        {/*<div class="col s12 m4 l2">
       </div>*/}
-      <div class="col s12 m8 l9">
-        {/*center */}
-        <div className="contenedorcentralGaming">
-      
-              <img src={logo}   className="imagenLogoGaming zoom-on-load"/>
-              <br/>
-              <p className="tituloParrafo slide-in-animate">
-                Armada para jugar 
-              </p>
-              <p className="textoCenterGaming slide-in-animate">
-                Sector dedicado para personas que quieren una 
-                experiencia de juego semiprofesional o profesional. 
-                Este sector está dedicado para ti
-
-              </p>
-              <br/>
-
-              
-              <a href="/"  class="botonGaming zoom-on-load">Cotizar</a>
-              <br/>
-              <p className="flecha zoom-on-load"></p> *
+        <div class="col s12 m8 l9">
+          {/*center */}
+          <div className="contenedorcentralGaming">
+            <img src={logo} className="imagenLogoGaming zoom-on-load" />
+            <br />
+            <p className="tituloParrafo slide-in-animate">Armada para jugar</p>
+            <p className="textoCenterGaming slide-in-animate">
+              Sector dedicado para personas que quieren una experiencia de juego
+              semiprofesional o profesional. Este sector está dedicado para ti
+            </p>
+            <br />
+            <a href="#cotizar-gaming" className="botonGaming zoom-on-load">
+              Cotizar
+            </a>
+            <br />
+            <p className="flecha zoom-on-load"></p> *
+          </div>
         </div>
-      </div>
-      <div class="col s12 m4 l3 no-padding">
-        {/*right */}
-        <div class="row">
-          <div class="col s8 offset-s2 m12 l12 no-padding">
-            <span className="titulo-galeria slide-in-animate-right">
-              Galería
-            </span>
-            <img className="linea-galeria zoom-on-load" src={linea} />
-            <img className="control-galeria zoom-on-load" src={control} />
-            <img className="control-galeria zoom-on-load" src={control} />
+        <div className="col s12 m4 l3 no-padding">
+          {/*right */}
+          <div class="row">
+            <div className="col s8 offset-s2 m12 l12 no-padding">
+              <span className="titulo-galeria slide-in-animate-right">
+                Galería
+              </span>
+              <img className="linea-galeria zoom-on-load" src={linea} />
+              <img className="control-galeria zoom-on-load" src={control} />
+              <img className="control-galeria zoom-on-load" src={control} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-      )
-}
 
+      <div
+        id="cotizar-gaming"
+        className="row"
+        style={{ paddingLeft: "5.5%", paddingRight: "2%", height: "500px" }}
+      >
+        <div className="form-container col s12 m8 offset-m2 l6 offset-l3">
+          <div className="row my-row">
+            <TabsGaming></TabsGaming>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
-  export default Gaming;
+export default Gaming;
